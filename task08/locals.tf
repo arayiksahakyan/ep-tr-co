6f6c7df1743721826c5e4e23333b497ea2535749
arayiksahakyan <arayiksahakyan59@gmail.com>
2026-07-05 03:51:06 +0400

    Use expected AKS manifest names

--- task08/locals.tf
+++ task08/locals.tf
@@ -10,8 +10,8 @@ locals {
   aci_dns_name_label         = lower(local.aci_name)
   app_image_name             = "${var.docker_image_name}:${var.image_tag}"
   container_name             = var.docker_image_name
-  kubernetes_deployment_name = var.docker_image_name
-  kubernetes_secret_name     = "redis-secret"
-  kubernetes_service_name    = var.docker_image_name
-  secret_provider_class_name = "azure-kvname"
+  kubernetes_deployment_name = "${var.docker_image_name}-deployment"
+  kubernetes_secret_name     = "${var.docker_image_name}-redis"
+  kubernetes_service_name    = "${var.docker_image_name}-service"
+  secret_provider_class_name = "${var.docker_image_name}-kv-secrets"
 }
